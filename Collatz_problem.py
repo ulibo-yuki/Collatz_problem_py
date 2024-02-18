6467d9e7831cc702d0baf0e4041abf283c0ff2ab
yuki <fu08yu20ko07@icloud.com>
2024-02-19 02:04:30 +0900

    グラフ機能の追加、それに伴うファイルパス管理の変更

--- Collatz_problem.py
+++ Collatz_problem.py
@@ -1,19 +1,22 @@
 import datetime
 import sys
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 while True:
     #パスの設定
-    log_file_name = f"log_{datetime.datetime.now().strftime('%m_%d_%H:%M:%S')}.csv"
+    datetime_now = datetime.datetime.now().strftime('%m_%d_%H:%M:%S')
+    log_file_name = f"log_{datetime_now}"
     if getattr(sys, 'frozen', False):
         # parent_path = os.path.dirname(sys.executable)
-        os.makedirs(f"{os.path.dirname(sys.executable)}/csv_files", exist_ok=True)
-        log_file_name = f"{os.path.dirname(sys.executable)}/csv_files/{log_file_name}"
+        os.makedirs(f"{os.path.dirname(sys.executable)}/csv_files/{log_file_name}", exist_ok=True)
+        log_file_name = f"{os.path.dirname(sys.executable)}/csv_files/{log_file_name}/{log_file_name}.csv"
         with open(log_file_name, 'w') as f:
             f.write("time,value\n")
     else:
-        os.makedirs("csv_files", exist_ok=True)
-        log_file_name = f"csv_files/{log_file_name}"
+        os.makedirs(f"csv_files/{log_file_name}", exist_ok=True)
+        log_file_name = f"csv_files/{log_file_name}/{log_file_name}.csv"
         with open(log_file_name, 'w') as f:
             f.write("time,value\n")
     #演算
@@ -29,12 +32,24 @@ while True:
             n /= 2
         else: #奇数
             n *= 3
-            n =+ 1
+            n += 1
         with open(log_file_name, 'a') as f:
             count += 1
             f.write(str(count) + "," + str(n) + "\n")
             print("\r" + str(count) + "回書き込み", end="")
     print(f"\n{n}になりました")
+    #グラフ
+    input_csv = pd.read_csv(log_file_name)
+    # save_path = f"{os.path.dirname(log_file_name)}"
+    plt.plot(input_csv["time"], input_csv["value"])
+    plt.xlabel("time")
+    plt.ylabel("value")
+    plt.savefig(f"{os.path.dirname(log_file_name)}/graph_{datetime_now}.png", format="png", dpi=300)
+    print(f"{os.path.dirname(log_file_name)}に画像を保存しました。")
+    print("グラフを表示しますか?(y/n)")
+    graph_show = input()
+    if graph_show == "y":
+        plt.show()
     print("もう一度実行しますか?(y/n)")
     if input() != "y":
         break
