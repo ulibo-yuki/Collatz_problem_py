48fbc5daeb57e9dafe8eae6e6a6f8d787bae42e9
yuki <fu08yu20ko07@icloud.com>
2024-03-10 01:10:04 +0900

    冗長なコードの削除と変数の振り分けの最適化

--- Collatz_problem.py
+++ Collatz_problem.py
@@ -6,22 +6,23 @@ import matplotlib.pyplot as plt
 
 while True:
     #パスの設定
-    datetime_now = datetime.datetime.now().strftime('%m_%d_%H:%M:%S')
-    log_file_name = f"log_{datetime_now}"
+    date_now = datetime.datetime.now().strftime('%m_%d_%H:%M:%S')
     if getattr(sys, 'frozen', False):
-        os.makedirs(f"{os.path.dirname(sys.executable)}/csv_files/{log_file_name}", exist_ok=True)
-        log_file_name = f"{os.path.dirname(sys.executable)}/csv_files/{log_file_name}/{log_file_name}.csv"
+        os.makedirs(f"{os.path.dirname(sys.executable)}/csv_files/log_{date_now}", exist_ok=True)
+        log_csv_file_path = f"{os.path.dirname(sys.executable)}/csv_files/log_{date_now}/log_{date_now}.csv"
     else:
-        os.makedirs(f"csv_files/{log_file_name}", exist_ok=True)
-        log_file_name = f"csv_files/{log_file_name}/{log_file_name}.csv"
-    with open(log_file_name, 'w') as f:
+        os.makedirs(f"csv_files/log_{date_now}", exist_ok=True)
+        log_csv_file_path = f"csv_files/log_{date_now}/log_{date_now}.csv"
+    with open(log_csv_file_path, 'w') as f:
         f.write("time,value\n")
     #演算
     print("値を入力してください")
     n = int(input())
     repeat = False
     count = 1
-    with open(log_file_name, 'a') as f:
+    with open(f"{os.path.dirname(log_csv_file_path)}/operation_log.txt", 'w') as f:
+        f.write(f"実行日時:{date_now}\n初期値:{n}\n")
+    with open(log_csv_file_path, 'a') as f:
         f.write(str(count) + "," + str(n) + "\n")
         print("\r" + str(count) + "回書き込み", end="")
     while n != 1:
@@ -30,18 +31,20 @@ while True:
         else: #奇数
             n *= 3
             n += 1
-        with open(log_file_name, 'a') as f:
+        with open(log_csv_file_path, 'a') as f:
             count += 1
             f.write(str(count) + "," + str(n) + "\n")
             print("\r" + str(count) + "回書き込み", end="")
     print(f"\n{n}になりました")
     #グラフ
-    input_csv = pd.read_csv(log_file_name)
+    input_csv = pd.read_csv(log_csv_file_path)
+    with open(f"{os.path.dirname(log_csv_file_path)}/operation_log.txt", 'a') as f:
+        f.write(f"演算回数:{count}\n最大値:{input_csv['value'].max()}\n最小値:{input_csv['value'].min()}")
     plt.plot(input_csv["time"], input_csv["value"])
     plt.xlabel("time")
     plt.ylabel("value")
-    plt.savefig(f"{os.path.dirname(log_file_name)}/graph_{datetime_now}.png", format="png", dpi=300)
-    print(f"{os.path.dirname(log_file_name)}に画像を保存しました。")
+    plt.savefig(f"{os.path.dirname(log_csv_file_path)}/graph_{date_now}.png", format="png", dpi=300)
+    print(f"{os.path.dirname(log_csv_file_path)}にグラフ画像を保存しました。")
     print("グラフを表示しますか?(y/n)")
     graph_show = input()
     if graph_show == "y":
